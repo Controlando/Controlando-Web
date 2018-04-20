25c9598dcd08f64b41aecea4b3cfc24bc547ab77
Daniel Figueira Ferreira <danielfigueira31@gmail.com>
2018-04-20 20:55:44 -0300

    Login e Cadastro
    Ambos possuem suas respectivas rotas implementadas, e uma leve introdução ao bootstrap, alguém deixa isso mais bonito aaaa

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import LandPage from './views/landPage/LandPage';
 import registerServiceWorker from './registerServiceWorker';
 import Login from './views/login/Login';
+import CreateAccount from './views/createAccount/CreateAccount'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 //import Login from './views/login/Login'
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter>  
         <Switch>
             <Route path = "/" exact={true} component= {LandPage}/>
             <Route path = "/Login" component = {Login}/>
+            <Route path = "/Cadastro" component ={CreateAccount}/>
         </Switch>
     </BrowserRouter>
     , document.getElementById('root'));
